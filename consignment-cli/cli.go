569d94e7226b520bfddc32f8580b60a5c10743de
tfhnia <tfhnia@gmail.com>
2019-11-04 23:09:54 +0800

    添加货船服务

--- consignment-cli/cli.go
+++ consignment-cli/cli.go
@@ -34,10 +34,10 @@ func parseFile(fileName string) (*pb.Consignment, error) {
 
 func main() {
 
-	cmd.Init()
+	err := cmd.Init()
 
 	// 服务发现 => pb.NewShippingService TODO: 这个方法封装了什么？
-	client := pb.NewShippingService("go.micro.srv.consignment", microclient.DefaultClient)
+	client := pb.NewShippingService("consignment", microclient.DefaultClient)
 
 	// 可以在命令行中指定 json 文件
 	infoFile := DEFAULT_INFO_FILE
--- consignment-service/main.go
+++ consignment-service/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	pb "github.com/ChenHanZhang/microservices-in-golang/proto/consignment"
+	vesselProto "github.com/ChenHanZhang/microservices-in-golang/proto/vessel"
 	micro "github.com/micro/go-micro"
+	"log"
 
 	"context"
 	"fmt"
@@ -12,29 +14,34 @@ const (
 	PORT = ":50051"
 )
 
-type IRepository interface {
+type Repository interface {
 	Create(consignment *pb.Consignment) (*pb.Consignment, error) // 存放新货物
+	GetAll() []*pb.Consignment
 }
 
 // 存放多批货物的仓库，实现 IRepository 接口
-type Repository struct {
+type ConsignmentRepository struct {
 	consignments []*pb.Consignment
 }
 
 
 // 相当于定义类方法
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
 	repo.consignments = append(repo.consignments, consignment)
 	return consignment, nil
 }
 
-func (repo *Repository) GetAll() []*pb.Consignment {
+func (repo *ConsignmentRepository) GetAll() []*pb.Consignment {
 	return repo.consignments
 }
 
 // 定义微服务, 要实现 ShippingServer 接口
 type service struct {
+	// 这里使用接口，TODO: 其实直接用 ConsignmentRepository 也是可以的
 	repo Repository
+
+	// 这里注册(记录)一个货船服务的客户端对象
+	vesselClient vesselProto.VesselServiceClient
 }
 
 // service 实现 consignment.pb.go 中的 ShippingServiceServer 接口
@@ -42,6 +49,22 @@ type service struct {
 // 就是这个CreateConsignment方法，它接受一个context以及proto中定义的
 // TODO: Consignment消息，这个Consignment是由 gRPC 的服务器处理后提供给你的
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+
+	// 这里，我们通过货船服务的客户端对象 TODO: 客户端对象?和之前在cli中的有什么不同
+	// 向货船服务发起一个请求
+	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
+		Capacity:             req.Weight,
+		MaxWeight:            int32(len(req.Containers)),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
+
+	req.VesselId = vesselResponse.Vessel.Id
+
 	// 接收承运的货物
 	consignment, err := s.repo.Create(req)
 	if err != nil {
@@ -60,17 +83,21 @@ func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *
 }
 
 func main() {
-	repo := Repository{}
+	repo := &ConsignmentRepository{}
 
 	// 注意，这里采用 go-micro 来实现
 	srv := micro.NewService(
-			micro.Name("go.micro.srv.consignment"),
+			micro.Name("consignment"),
 			micro.Version("latest"),
 			)
 
+	// 在这里使用预置的方法生成一个货船服务的客户端对象
+	vesselclient := vesselProto.NewVesselServiceClient("go.micro.srv.vessel", srv.Client())
+
 	srv.Init()
 
-	err := pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+	// TODO: 这里的 service 和 pb 的(依赖)关系
+	err := pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselclient})
 
 	if err = srv.Run(); err != nil {
 		fmt.Println(err)
